99cea9db3543fb9f09ec8f84bc0acacd1989cdc0
Chiakikun <chiakikungm@gmail.com>
2021-01-28 00:50:49 +0900

    タイトルが試作名だったので修正

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -338,7 +338,7 @@
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "MainForm";
-            this.Text = "CustomChart";
+            this.Text = "TrainingFXChart";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
